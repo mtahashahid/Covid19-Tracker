f70576e2e535e6a56353344007c7cd2c2aa581b9
Muhammad Taha <taha_shahid@outlook.com>
2021-05-17 12:24:16 +0500

    initial commit

---
+++ src/Containers/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <nav>
+          <div className="nav-wrapper #e53935 red darken-1">
+            <a href="!#" className="brand-logo center " style={{fontFamily: 'Fredoka One, cursive'}}>
+              COVID-19 Tracker
+            </a>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+export default Header;
